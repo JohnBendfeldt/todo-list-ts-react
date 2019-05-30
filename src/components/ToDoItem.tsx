74a58aea81ecffa5c0c527860b35ae6d85387f92
John Bendfeldt <jfbendfeldt@gmail.com>
2019-05-30 14:15:36 -0500

    Changes css for delete icon

--- src/components/ToDoItem.tsx
+++ src/components/ToDoItem.tsx
@@ -1,43 +1,37 @@
-import React, { Component, CSSProperties } from 'react';
+import React, { Component } from 'react';
 import DeleteIcon from '@material-ui/icons/Delete';
 import Checkbox from '@material-ui/core/Checkbox';
 import Card from '@material-ui/core/Card';
 import { Props } from '../types';
 
 interface ToDoItemProps extends Props {
-  toDo: { id: string; title: string; completed: boolean };
+    toDo: { id: string; title: string; completed: boolean };
 }
 
 export default class ToDoItem extends Component<ToDoItemProps> {
-  getStyle = () => {
-    return {
-      textDecoration: this.props.toDo.completed ? 'line-through' : 'none',
-      margin: '5px 5px'
+    getStyle = () => {
+        return {
+            textDecoration: this.props.toDo.completed ? 'line-through' : 'none',
+            margin: '5px 5px'
+        };
     };
-  };
 
-  render() {
-    const { id, title } = this.props.toDo;
-    return (
-      <Card style={this.getStyle() as any}>
-        <p>
-          <Checkbox
-            checked={this.props.toDo.completed}
-            onChange={this.props.markComplete.bind(this, id)}
-          />
-          {title}
-          <DeleteIcon
-            onClick={this.props.deleteToDo.bind(this, id)}
-            style={iconStyle}
-          />
-        </p>
-      </Card>
-    );
-  }
+    render() {
+        const { id, title } = this.props.toDo;
+        return (
+            <Card style={this.getStyle() as any}>
+                <p>
+                    <Checkbox
+                        checked={this.props.toDo.completed}
+                        onChange={this.props.markComplete.bind(this, id)}
+                    />
+                    {title}
+                    <DeleteIcon
+                        onClick={this.props.deleteToDo.bind(this, id)}
+                        className='iconStyle'
+                    />
+                </p>
+            </Card>
+        );
+    }
 }
-
-const iconStyle: CSSProperties = {
-  margin: '10px 10px 0px 0px',
-  cursor: 'pointer',
-  float: 'right'
-};
