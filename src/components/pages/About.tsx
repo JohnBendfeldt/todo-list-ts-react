bec3ac3ffddb44c828ac0cbbaacd7557703c0c74
John Bendfeldt <jfbendfeldt@gmail.com>
2019-05-29 22:22:46 -0500

    Fixes a typo, adds styling and more description for the About page

--- src/components/pages/About.tsx
+++ src/components/pages/About.tsx
@@ -7,8 +7,29 @@ export default function About() {
             <Card style={{ padding: '10px 15px', margin: '5px 5px' }}>
                 <h1>About</h1>
                 <p>
-                    This is the To Do List App v1.0.0. Is is built using React
-                    with TypeScript.
+                    This is the To Do List App v1.0.0.
+                    <br />
+                    It is built using React with TypeScript.
+                    <br />
+                    Your to do list items are stored in local storage thanks to{' '}
+                    <a
+                        href='https://www.npmjs.com/package/react-simple-storage'
+                        target='_blank'
+                        rel='noopener noreferrer'
+                    >
+                        React Simple Storage
+                    </a>
+                    .
+                    <br />
+                    The source code can be found{' '}
+                    <a
+                        href='https://github.com/JohnBendfeldt/todo-list-ts-react'
+                        target='_blank'
+                        rel='noopener noreferrer'
+                    >
+                        here
+                    </a>
+                    .
                 </p>
             </Card>
         </React.Fragment>
