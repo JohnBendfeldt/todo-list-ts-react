3bf8f260c46003acd33610e60784d3a2b9ca687e
John Bendfeldt <jfbendfeldt@gmail.com>
2019-05-31 23:39:01 -0500

    Ensures SSL use on heroku

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
 const express = require('express');
 const http = require('http');
-var enforce = require('express-sslify');
 const path = require('path');
 let app = express();
-app.use(enforce.HTTPS(), express.static(path.join(__dirname, 'build')));
+const ensureSecure = (req, res, next) => {
+    if (
+        req.get('X-Forwarded-Proto') == 'https' ||
+        req.hostname == 'localhost'
+    ) {
+        next();
+    } else if (
+        req.get('X-Forwarded-Proto') != 'https' &&
+        req.get('X-Forwarded-Port') != '443'
+    ) {
+        res.redirect('https://' + req.hostname + req.url);
+    }
+};
+app.use(express.static(path.join(__dirname, 'build')), ensureSecure);
 app.get('/*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
 });
